298ddb956df0ec5ccbea22e8a07147942857f2a3
baillodev <mamadoubailodiallo952@gmail.com>
2026-02-21 08:52:47 +0000

    remove: authentication system from frontend

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,12 +2,10 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
-import 'package:titanic_app/providers/auth_provider.dart';
 import 'package:titanic_app/providers/prediction_provider.dart';
 import 'package:titanic_app/services/api_service.dart';
-import 'package:titanic_app/services/auth_service.dart';
 import 'package:titanic_app/utils/constants.dart';
-import 'package:titanic_app/widgets/auth_check_widget.dart';
+import 'package:titanic_app/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,40 +15,23 @@ void main() async {
   setupEnv();
 
   final sharedDio = Dio(BaseOptions(baseUrl: apiBaseUrl));
-
-  final authService = AuthService(sharedDio);
-  final apiService = ApiService(sharedDio, authService);
-
-  final authProvider = AuthProvider(authService);
+  final apiService = ApiService(sharedDio);
   final predictionProvider = PredictionProvider(apiService);
 
-  runApp(
-    MyApp(authProvider: authProvider, predictionProvider: predictionProvider),
-  );
+  runApp(MyApp(predictionProvider: predictionProvider));
 }
 
 class MyApp extends StatelessWidget {
-  final AuthProvider authProvider;
   final PredictionProvider predictionProvider;
 
-  const MyApp({
-    super.key,
-    required this.authProvider,
-    required this.predictionProvider,
-  });
+  const MyApp({super.key, required this.predictionProvider});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
-        ChangeNotifierProvider<PredictionProvider>.value(
-          value: predictionProvider,
-        ),
-      ],
-
+    return ChangeNotifierProvider<PredictionProvider>.value(
+      value: predictionProvider,
       child: MaterialApp(
-        home: AuthCheckWidget(),
+        home: const HomeScreen(),
         debugShowCheckedModeBanner: false,
       ),
     );
